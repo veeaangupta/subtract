fce7b2fdce0a17b7252087d93f63ea668bec6114
veeaangupta <95080000+veeaangupta@users.noreply.github.com>
2021-11-26 16:59:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function subtract(){
+var answer=Number( document.getElementById("first").value) -Number( document.getElementById("second").value)
+
+document.getElementById("answer").innerHTML =answer
+}
